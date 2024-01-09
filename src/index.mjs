634519b0b73914dfafd5b68b6869b60af2fbd2eb
Shaurya Jha <shauryajha1602@gmail.com>
2024-01-09 11:51:44 +0530

    understood GET requests

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,25 @@ const port = 8000
 
 const app = express();
 
+// routes
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome')
+})
+
+app.get('/api/users', (req, res) => {
+    res.send([
+        {id:1, name:'anson', displayName:'Anson'},
+        {id:2, name:'jack', displayName:'Jack'},
+        {id:3, name:'adam', displayName:'Adam'},
+    ])
+})
+
+app.get('/api/products', (req,res) => {
+    res.send([
+        {id: 123, name:'chicken breast', price:12.99},
+    ])
+})
+
 app.listen(port, () => {
     console.log(`server running on port: ${port}`);
 })
